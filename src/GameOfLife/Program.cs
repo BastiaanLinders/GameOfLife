7d16459ad88ec9263aeb20f137ecaa36bead0fe3
Bastiaan Linders <bastiaan.linders@gmail.com>
2024-12-19 21:38:39 +0100

    Moved code to src

---
+++ src/GameOfLife/Program.cs
@@ -0,0 +1,15 @@
+﻿using GameOfLife;
+using GameOfLife.Services.Config;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StackExchange.Profiling;
+
+MiniProfiler.StartNew();
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddGameOfLifeServices();
+builder.Services.AddHostedService<Worker>();
+
+using var host = builder.Build();
+host.Run();
